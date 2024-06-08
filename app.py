8681d639605c9a34bdabdb772ca67e201e0e21db
deathnote2501 <107842385+deathnote2501@users.noreply.github.com>
2024-06-08 15:44:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,12 +19,14 @@ if not api_key:
 client = OpenAI(api_key=api_key)
 
 # Streamlit interface
-st.title("Pascaleo")
-st.subheader("Retranscription textuelle des entretiens visios")
-st.write("\n")
-whisper_prompt = st.text_area("Entrez les termes techniques issus du GPTs #4", "")
-st.write("\n")
-uploaded_files = st.file_uploader("Téléversez vos fichiers MP3 pour obtenir une retranscription textuelle au format TXT et PDF", type="mp3", accept_multiple_files=True)
+st.title("Pascaleo - Retranscription textuelle des entretiens visios")
+st.markdown("<hr>", unsafe_allow_html=True)
+st.subheader("Entrez les termes techniques issus du GPTs #4 :")
+whisper_prompt = st.text_area("", "")
+st.markdown("<hr>", unsafe_allow_html=True)
+st.subheader("Téléversez vos fichiers MP3 :")
+uploaded_files = st.file_uploader("", type="mp3", accept_multiple_files=True)
+st.markdown("<hr>", unsafe_allow_html=True)
 
 if st.button("Retranscrire les MP3 en PDF"):
     if uploaded_files:
@@ -87,13 +89,13 @@ if st.button("Retranscrire les MP3 en PDF"):
             st.write(f"Processed {uploaded_file.name}")
 
             # Provide download link for the text file
-            with open(output_txt_path, "r") as file:
-                st.download_button(
-                    label="Télécharger les retranscriptions (TXT)",
-                    data=file,
-                    file_name=f"{Path(uploaded_file.name).stem}.txt",
-                    mime="text/plain"
-                )
+            # with open(output_txt_path, "r") as file:
+            #     st.download_button(
+            #         label="Télécharger les retranscriptions (TXT)",
+            #         data=file,
+            #         file_name=f"{Path(uploaded_file.name).stem}.txt",
+            #         mime="text/plain"
+            #     )
 
             # Provide download link for the PDF file
             with open(output_pdf_path, "rb") as file:
